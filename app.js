b5ebd3c0fea5e974696c8f8069cf8c63c9020122
MaccSob <71sobmaciej@gmail.com>
2025-06-11 23:49:47 +0200

    adsad

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+require('dotenv').config()
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+
+app.delete('/users', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
+);
